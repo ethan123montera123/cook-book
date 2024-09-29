eaba87ad9ec5e071f6461111260848ae041cebd1
alphas12 <146078143+alphas12@users.noreply.github.com>
2024-09-29 19:32:50 +0800

    init

--- cook-book/tailwind.config.js
+++ cook-book/tailwind.config.js
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
     content: [
         "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                dominant: '#D8D2C2',  // Light Greyish Tone
+                primary: '#4A4947',   // Medium Dark
+                accent: '#B17457',    // Accent Color
+                light: '#FAF7F0'      // Background Light Color
+             },
+
+            fontFamily: {
+                heading: ['Playfair Display', 'serif'], // For headers
+                body: ['Open Sans', 'sans-serif'],      // For body text
+            },
+        },
     },
     plugins: [],
 }
